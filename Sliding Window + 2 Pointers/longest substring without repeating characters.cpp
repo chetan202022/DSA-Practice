ee7f2bc4b84997d25869e7923373f4df9b3d283c
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-01-06 22:46:23 +0530

    Added solution for longest substring without repeating characters
    Implement the sliding window technique to find the length of the longest substring without repeating characters.

---
+++ Sliding Window + 2 Pointers/longest substring without repeating characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> last(256, -1);
+
+        int l=0, ans=0;
+
+        for(int r=0; r<s.size(); r++){
+            if(last[s[r]] >= l){
+                l = last[s[r]] + 1;
+            }
+            last[s[r]] = r;
+            ans = max(ans, r-l+1);
+        }
+        return ans;
+    }
+};
