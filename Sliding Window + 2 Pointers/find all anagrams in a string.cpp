acf8ab7dd175cd587489a5822c18f77faedf74f9
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-01-06 22:47:34 +0530

    Implemented function to find anagrams in a string

---
+++ Sliding Window + 2 Pointers/find all anagrams in a string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        if(s.size() < p.size()) return ans;
+
+        vector<int> cntS(26, 0), cntP(26, 0);
+
+        for(char c : p) cntP[c - 'a']++;
+
+        int k = p.size();
+        for(int i=0; i<s.size(); i++){
+            cntS[s[i] - 'a']++;
+
+            if(i >= k) cntS[s[i-k] - 'a']--;
+
+            if(cntS == cntP) ans.push_back(i - k + 1);
+        }
+        return ans;
+    }
+};
