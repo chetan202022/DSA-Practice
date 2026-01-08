8a8e340245688d7d9df8b7c3158453103abd7e89
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-01-08 22:40:33 +0530

    Implemented maxArea function for container with most water
    Added implementation for maxArea function to calculate the maximum area of water that can be contained.

---
+++ Sliding Window + 2 Pointers/container with most water.cpp
@@ -0,0 +1,16 @@
+
+// https://leetcode.com/problems/container-with-most-water/description/
+
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int l=0, r=h.size()-1;
+        int area=0;
+        while(l < r){
+            area = max(area, (r-l)*min(h[l], h[r]));
+            if(h[l] < h[r]) l++;
+            else r--;
+        }
+        return area;
+    }
+};
