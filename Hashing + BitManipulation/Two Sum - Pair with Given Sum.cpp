0842b37a3ac743e6644d09df38caf96a2b812fec
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-03-16 20:15:39 +0530

    Added twoSum function for pair with given sum
    Implement twoSum function to find if there exists a pair that sums to the target.

---
+++ Hashing + BitManipulation/Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,17 @@
+
+https://www.geeksforgeeks.org/problems/key-pair5616/1
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        unordered_map<int, int> mp;
+        for(auto &a : arr) mp[a]++;
+        
+        for(auto &a : arr){
+            if(mp.find(target-a) != mp.end()){
+                if((target-a) != a || mp[a] > 1) return true;
+            }
+        }
+        return false;
+    }
+};
