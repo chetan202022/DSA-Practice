90dd80a3d881cbd6a07105d25ce3ce2cf4bc08f9
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-03-19 17:28:45 +0530

    Implemented Gray Code generation algorithm

---
+++ Hashing + BitManipulation/Gray Code.cpp
@@ -0,0 +1,14 @@
+
+// https://leetcode.com/problems/gray-code/
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+
+        for(int i=0; i<(1<<n); i++){
+            ans.push_back(i ^ (i >> 1));
+        }
+        return ans;
+    }
+};
