7869fe28d237d72576c6474076d793aedcc64b90
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-03-26 13:33:51 +0530

    Created Sum of the products of all possible Subsets.cpp

---
+++ DP/Sum of the products of all possible Subsets.cpp
@@ -0,0 +1,24 @@
+
+// https://www.geeksforgeeks.org/dsa/sum-products-possible-subsets/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns sum of products of all subsets
+// of arr[0..n-1]
+int productOfSubsetSums(int arr[], int n)
+{
+    int ans = 1;
+    for (int i = 0; i < n; ++i )
+        ans = ans * (arr[i] + 1);
+    return ans-1;
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof arr[0];
+    cout << productOfSubsetSums(arr, n);
+    return 0;
+}
