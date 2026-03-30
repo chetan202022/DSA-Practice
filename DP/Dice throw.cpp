43e3f2e279b33d1fe57c8fba0e8bb68bc102d875
Chetan <143815743+chetan202022@users.noreply.github.com>
2026-03-30 23:13:46 +0530

    Created Dice throw.cpp

---
+++ DP/Dice throw.cpp
@@ -0,0 +1,26 @@
+
+// https://www.geeksforgeeks.org/problems/dice-throw5349/1
+
+class Solution {
+  public:
+    int f(int m, int n, int x, vector<vector<int>>& dp){
+        if(n == 0 && x == 0) return 1;
+        if(n == 0 || x <= 0) return 0;
+        
+        if(dp[n][x] != -1) return dp[n][x];
+        
+        int ans=0;
+        
+        for(int i=1; i<=m; i++){
+            ans += f(m, n-1, x-i, dp);
+        }
+        
+        return dp[n][x] = ans;
+    }
+  
+    int noOfWays(int m, int n, int x) {
+        vector<vector<int>> dp(n+1, vector<int>(x+1, -1));
+        return f(m, n, x, dp);
+        
+    }
+};
